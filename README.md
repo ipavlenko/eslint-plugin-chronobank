7dfd6d59425b4ed196de214da12139ea4b0e03fa
Igor Pavlenko <ipavlenko@inbox.ru>
2017-07-04 23:37:36 +0500

    Code style

---
+++ README.md
@@ -0,0 +1,18 @@
+# eslint-plugin-chronobank
+
+> ESLint plugin for the ChronoBank related projects.
+
+## Usage
+
+1. `npm install eslint-plugin-chronobank --save-dev`
+2. create a file named `.eslintrc` in your project:
+
+```js
+{
+  extends: ["plugin:chronobank/recommended"]
+}
+```
+
+## License
+
+[GPL-3.0](https://opensource.org/licenses/GPL-3.0)
---
+++ config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  'extends': ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: [
+      'react',
+      'async-await'
+  ],
+  parserOptions: {
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  parser: 'babel-eslint',
+  env: {
+    es6: true,
+    node: true
+    browser: true,
+    jest: true,
+    commonjs: true
+  },
+  globals: {
+    document: false,
+    window: false
+  },
+  rules: {
+    "semi": ["error", "never"],
+    "no-console": 1,
+    "no-unused-vars": 1,
+    "no-case-declarations": 1,
+    'curly': [2, 'multi-line'],
+    'dot-location': [2, 'property'],
+    'eol-last': 2,
+    'eqeqeq': [2, 'always', {'null': 'ignore'}],
+    'handle-callback-err': [2, '^(err|error)$' ],
+    'indent': [2, 2, { 'SwitchCase': 1 }],
+    'jsx-quotes': [2, 'prefer-single'],
+    "react/jsx-key": 1,
+    "react/prop-types": 1,
+    "react/no-string-refs": 1,
+    "react/display-name": 1,
+    "react/no-unescaped-entities": 1,
+    "react/jsx-no-target-blank": 1,
+    "react/no-find-dom-node": 1,
+    "react/no-deprecated": 1,
+  }
+}
